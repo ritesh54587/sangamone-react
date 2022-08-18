ab59719d813f1cb26836eb8156f9d7f87d58069a
ritesh54587 <riteshkumaryadav.sangamone@gmail.com>
2022-08-18 23:51:38 +0530

    add button

--- src/List.js
+++ src/List.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function List() {
+    const click = (a) => {
+        alert(a);
+      }
    return (
     <ol>
         <li>Mobiles</li>
         <li>TVs and Radio</li>
         <li>Chargers</li>
         <li>EarPhones</li>
+        <button onClick={()=> click('Thank you')}>Click Me</button>
+
     </ol>
    );
 }
